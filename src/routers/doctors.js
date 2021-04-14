05f2de5ad06a6b19eb38e27697fc9639f79009bf
Namrata Thaker <namrata.t@simformsolutions.com>
2021-04-14 16:12:08 +0530

    doctorapi

--- src/routers/doctors.js
+++ src/routers/doctors.js
@@ -3,20 +3,32 @@ const router = express.Router();
 const Doctor = require("../models/doctor");
 const Clinic = require("../models/clinic");
 const auth = require("../middlewares/doctorAuth");
+const e = require("express");
 
 router.get("/doctors", async (req, res) => {
   const city = req.query.city;
+  const speciality = req.query.doctor_speciality;
+  if(city&&speciality){
   Clinic.find({ "clinic_address.city": city }).then(async(clinic) => {
     if (clinic) {
-      const resu=await Promise.all(clinic.map(async (val)=>{
-        const doctor= await Doctor.find({_id:val.owner});
+      const result=await Promise.all(clinic.map(async (val)=>{
+        const doctor= await Doctor.find({"_id":val.owner , "doctor_speciality": speciality});
         return doctor[0];
       }))
-      res.send(resu);
+      if(result.length!=0){
+        res.send(result);
+      }
+      else{
+          res.status(400).send()
+      }
     }else{
       console.log("clinic not found");
     }
   });
+}
+else{
+  res.send('enter both values')
+}
 });
 
 
